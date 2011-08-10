bbc9ca0a81f0de938ecfb4a6802d0532249ff1f5
leizhao4 <leizhao4@gmail.com>
2011-08-10 16:10:39 -0500

    created new rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arsbbs::Application.config.secret_token = 'd25a1d04959f75a9b3633587fd7d62ccb160637455db3143641270878205badce039e1024c566ddb1f2fab8cc20fd50cbbf569f63e93c9ebb56c34e2e28e897f'
