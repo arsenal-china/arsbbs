6353081a70faa9ca945347089aa73b4afd5f3b2e
leizhao4 <leizhao4@gmail.com>
2011-08-21 00:12:34 -0500

    changed locale from zh to zh-CN

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,4 +2,4 @@
 # I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
  
 # set default locale to something other than :en
-I18n.default_locale = :zh
+I18n.default_locale = "zh-CN"
