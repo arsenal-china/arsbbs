91d8f256eb4285776b75ef761b3ef572d405fcaa
Lei Zhao <leizhao4@gmail.com>
2011-12-25 21:56:07 -0600

    Show alpha message on home page; redirect from home to alpha page if not logged in; redirect from alpha to home page if logged in

--- app/controllers/alpha_controller.rb
+++ app/controllers/alpha_controller.rb
@@ -1,6 +1,9 @@
 class AlphaController < ApplicationController
 
   def index
+    if logged_in?
+      redirect_to :home
+    end
     @user = User.new
   end
 
--- test/functional/alpha_controller_test.rb
+++ test/functional/alpha_controller_test.rb
@@ -2,10 +2,16 @@ require 'test_helper'
 
 class AlphaControllerTest < ActionController::TestCase
   
-  test "should get index" do
+  test "should get alpha page if not logged in" do
     get :index
     assert_response :success
     assert_not_nil assigns[:user]
+  end
+
+  test "should redirect to home page if logged in" do
+    @controller.stubs(:current_user).returns(User.first)
+    get :index
+    assert_redirected_to home_url
   end
 
 end
