1b25d2fb377f7a8048f822e94921f2064e35d041
Lei Zhao <leizhao4@gmail.com>
2011-12-26 01:49:22 -0600

    Settings for asset precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Arsbbs::Application.configure do
   config.assets.css_compressor = :scss
   
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
