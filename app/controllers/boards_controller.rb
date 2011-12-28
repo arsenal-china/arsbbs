405dce6d6e6d309cd8d5c550ca0929da44f45595
Lei Zhao <leizhao4@gmail.com>
2011-12-28 17:57:31 -0600

    Removed JSON from board and topic controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,84 +1,44 @@
 class BoardsController < ApplicationController
-  # GET /boards
-  # GET /boards.json
   def index
     @boards = Board.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @boards }
-    end
   end
 
-  # GET /boards/1
-  # GET /boards/1.json
   def show
     @board = Board.find(params[:id])
     @topics = @board.topic
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @board }
-    end
   end
 
-  # GET /boards/new
-  # GET /boards/new.json
   def new
     @board = Board.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @board }
-    end
   end
 
-  # GET /boards/1/edit
   def edit
     @board = Board.find(params[:id])
   end
 
-  # POST /boards
-  # POST /boards.json
   def create
     @board = Board.new(params[:board])
 
-    respond_to do |format|
-      if @board.save
-        format.html { redirect_to @board, :notice => 'Board was successfully created.' }
-        format.json { render :json => @board, :status => :created, :location => @board }
-      else
-        format.html { render :action => "new" }
-        format.json { render :json => @board.errors, :status => :unprocessable_entity }
-      end
+    if @board.save
+      redirect_to @board, :notice => 'Board was successfully created.'
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /boards/1
-  # PUT /boards/1.json
   def update
     @board = Board.find(params[:id])
 
-    respond_to do |format|
-      if @board.update_attributes(params[:board])
-        format.html { redirect_to @board, :notice => 'Board was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @board.errors, :status => :unprocessable_entity }
-      end
+    if @board.update_attributes(params[:board])
+      redirect_to @board, :notice => 'Board was successfully updated.'
+    else
+      render :action => "edit"
     end
   end
 
-  # DELETE /boards/1
-  # DELETE /boards/1.json
   def destroy
     @board = Board.find(params[:id])
     @board.destroy
-
-    respond_to do |format|
-      format.html { redirect_to boards_url }
-      format.json { head :ok }
-    end
+    redirect_to boards_url
   end
 end
